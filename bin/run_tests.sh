b4cd1973dbd2b0871b002ff80b022745505344f7
Doman <pi.domanski@gmail.com>
2023-04-08 16:58:30 +0200

    well this tests worked

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -7,7 +7,7 @@ run_test()
 	local model=$1
 	local dataset=$2
 	local logfolder=$3
-	for folder in $dataset/*/*; do
+	for folder in $dataset/*; do
 		logfile=$logfolder/$(echo "$folder" | sed 's/\//_/g')
 		python -m enhancer -t -c $model -x $folder > $logfile
 	done
